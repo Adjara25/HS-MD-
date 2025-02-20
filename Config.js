e9e5cc04cdf31520213b62a4f016510fdc795cf3
Adjara25 <sougueadjara6@gmail.com>
2025-02-20 16:27:16 +0000

    Update Config.js

--- Config.js
+++ Config.js
@@ -6,7 +6,7 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 SESSION_ID: process.env.SESSION_ID || "2VVSSToI#XbauT9Vb0dl9827ui3fFqUYELdMiLvVA0kdQza6b5yI",
-ALIVE_IMG: process.env.ALIVE_IMG || "https://i.ibb.co/Qp22bZ1/IMG-20241129-WA0428.jpg",
+ALIVE_IMG: process.env.ALIVE_IMG || "https://i.ibb.co/Qp22bZ1/IMG-jordan-dunck.jpg",
 ALIVE_MSG: process.env.ALIVE_IMG || "*HEY DEAR* ${pushname}\n *I Am AlIVE NOW....! 🖐🏻*",
 SUDO_NB: process.env.SUDO_NB || "2250500525480",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
